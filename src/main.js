422f1f579d2d67bd1b4f05212c4e037f758a0536
elephantcastle <obymik89@gmail.com>
2021-05-15 21:11:47 +0200

    vue3, router, amplify auth setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
+import Amplify from 'aws-amplify';
 import { createApp } from 'vue';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
 import App from './App.vue';
 import router from './router';
+import awsExports from './aws-exports';
 
-createApp(App).use(router).mount('#app');
+Amplify.configure(awsExports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+const app = createApp(App).use(router);
+app.mount('#app');
